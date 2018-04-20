10135af9031c0a712892b3ba2ed7c32a061e8662
d6rkaiz <d6rkaiz@gmail.com>
2018-04-20 11:53:57 +0000

    add whitelist

--- conv.sh
+++ conv.sh
@@ -5,23 +5,27 @@ TMPZONEFILE=$ZONEFILE.tmp
 WORKFILE="blacklist-zone.work"
 
 wget -N https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts -O stevenblack.txt
-tail +30 stevenblack.txt | grep -v '^\s*$' | grep -v '^\s*#.*$' | grep -v '^#' | awk '{print $2}' | tr -s '\n' > $WORKFILE
+tail +30 stevenblack.txt | grep -v '^\s*#.*$' | grep -v '^#' | awk '{print $2}' | tr -s '\n' > $WORKFILE
 
 wget -N http://www.malwaredomainlist.com/hostslist/hosts.txt -O malwaredomainlist.txt
-tail +7 malwaredomainlist.txt | grep -v '^\s*$' | grep -v '^#' | awk '{print $2}' | tr -d '\r' | tr -s '\n' >> $WORKFILE
+tail +7 malwaredomainlist.txt | grep -v '^#' | awk '{print $2}' | tr -d '\r' | tr -s '\n' >> $WORKFILE
 
 wget -N http://someonewhocares.org/hosts/hosts -O someonewhocares.txt
-tail +80 someonewhocares.txt| grep -v '^\s*$' | grep -v '^\s*#.*$' | grep -v '^#' | awk '{print $2}' | tr -s '\n' >> $WORKFILE
+tail +80 someonewhocares.txt| grep -v '^\s*#.*$' | grep -v '^#' | awk '{print $2}' | tr -s '\n' >> $WORKFILE
 
 wget -N https://raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts -O coinblockerlist.txt
-tail +3 coinblockerlist.txt | grep -v '^\s*$' | grep -v '^#' | awk '{print $2}' | tr -d '\r' | tr -s '\n' >> $WORKFILE
+tail +3 coinblockerlist.txt | grep -v '^#' | awk '{print $2}' | tr -d '\r' | tr -s '\n' >> $WORKFILE
+
+cat whitelist.txt | while read LINE || [ -n "${LINE}" ]; do
+  sed -i "s/^${LINE}$//" $WORKFILE
+done
 
 echo '# Converted Blacklist from https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts' >> $TMPZONEFILE
 echo '# Converted Blacklist from http://www.malwaredomainlist.com/hostslist/hosts.txt' >> $TMPZONEFILE
 echo '# Converted Blacklist from http://someonewhocares.org/hosts/hosts' >> $TMPZONEFILE
 echo '# Converted Blacklist from https://raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts' >> $TMPZONEFILE
 echo '' >> $TMPZONEFILE
-cat $WORKFILE | tr A-Z a-z | rev | sort -n | uniq | rev | sed -e 's/^\(.*\)$/local-data: \"\1 A 127.0.0.1\"/' >> $TMPZONEFILE
+cat $WORKFILE | grep -v '^\s*$' | tr A-Z a-z | rev | sort -n | uniq | rev | sed -e 's/^\(.*\)$/local-data: \"\1 A 127.0.0.1\"/' >> $TMPZONEFILE
 
 echo '# my blacklist' >> $TMPZONEFILE
 # unbound wildcard
